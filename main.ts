40e3f921d79e889a8ff271d1cc4690fcbf2da2d2
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-11-08 08:06:45 -0800

    24-1108-0810 Add Copyright & Ver-Id *** Hold off on 'Flip "show led" block" upside-down for driving and hide 'Setup_Function' for minimalist

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"2-Sequence Animation Validates New Start"
+)
 basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"©️ 2024 Quest Institute. All rights reserved."
+)
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"Ver 2.1 (0.1.4, 24-1108-0800)"
+)
 basic.forever(function () {
 	
 })
