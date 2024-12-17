d8319dfb267943887ced41cd6dedcc658948889e
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2024-12-17 02:25:19 -0800

    robot100-blank_template-v2.1 (24-1217-0230): Add Comment Green Block

--- main.ts
+++ main.ts
@@ -4,11 +4,11 @@ quest_Note_1.quest_Show_String_For_Note_Small_Func(
 basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "©️ 2024 Quest Institute. All rights reserved."
 )
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
-"Ver 2.1 (0.1.5, 24-1108-0800)"
+quest_Note_2.quest_Show_String_For_Note_Big_Func(
+"Your Code Can Start Below"
 )
 basic.forever(function () {
 	
