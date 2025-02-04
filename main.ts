a4556c81dc260fa001bb74ba74e8d4ebd7bb1a21
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-02-04 08:31:18 -0800

    25-0204-0820 *** Toggable-Modularize 'on start' Stack w/ 'True' Statements for Maintenance *** Add 'set... for 2 sec' after Smiley Face *** Staff Setup-Code w/in 'on start' Stack ***  Copyright 2025 *** System Maintenance & GitHub Sync

--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
-"2-Sequence Animation Validates New Start"
-)
-basic.showIcon(IconNames.Heart)
-quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-basic.showIcon(IconNames.Happy)
-quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
-"Set 'rotate display to [upside down]'.."
-)
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
-"..to sync micro:bit w/'show leds' blocks"
-)
-display.rotateTo(display.Direction.UpsideDown)
+if (true) {
+    quest_Note_1.quest_Show_String_For_Note_Small_Func(
+    "2-Sequence Animation Validates New Start"
+    )
+    basic.showIcon(IconNames.Heart)
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+    basic.showIcon(IconNames.Happy)
+    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+}
+if (true) {
+    quest_Note_1.quest_Show_String_For_Note_Small_Func(
+    "Set 'rotate display to [upside down]'.."
+    )
+    quest_Note_1.quest_Show_String_For_Note_Small_Func(
+    "..to sync micro:bit w/'show leds' blocks"
+    )
+    display.rotateTo(display.Direction.UpsideDown)
+}
+if (true) {
+    quest_Note_1.quest_Show_String_For_Note_Big_Func(
+    "Staff Setup-Code can be in this 'True' Statement"
+    )
+}
 quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "©️ 2025 Quest Institute. All rights reserved."
 )
