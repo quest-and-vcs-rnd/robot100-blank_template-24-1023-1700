400d427361728aef6ecd7c5b1a776e6df7f4310c
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-02-11 04:42:47 -0800

    25-0211-0450 Upgrade to 'CT' Version

--- main.ts
+++ main.ts
@@ -1,31 +1,37 @@
-if (true) {
-    quest_Note_1.quest_Show_String_For_Note_Small_Func(
-    "2-Sequence Animation Validates New Start"
-    )
-    basic.showIcon(IconNames.Heart)
-    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-    basic.showIcon(IconNames.Happy)
-    quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+function setup_Staff_ThisLessonOnly_Func () {
+	
 }
-if (true) {
-    quest_Note_1.quest_Show_String_For_Note_Small_Func(
-    "Set 'rotate display to [upside down]'.."
+function setup_System_AnyLessonAlways_Func () {
+    quest_Dashboard.quest_Send_LoginOfBot_ToXrayDashboardOfMb_Func(
+    0,
+    quest_Toggle_OnOff_Enum.On,
+    quest_Debug_Show_Enum.Dashboard_OLED
     )
+}
+function setup_Student_ThisLessonOnly_Func () {
+	
+}
+function pre_setup_System_AnyLessonAlways_Func () {
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
-    "..to sync micro:bit w/'show leds' blocks"
+    "Fix 'show leds' Block's Orientation for Driving"
     )
     display.rotateTo(display.Direction.UpsideDown)
 }
-if (true) {
-    quest_Note_1.quest_Show_String_For_Note_Big_Func(
-    "Staff Setup-Code can be in this 'True' Statement"
-    )
-}
+pre_setup_System_AnyLessonAlways_Func()
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"2-Sequence Animation Validates New-Start"
+)
+basic.showIcon(IconNames.Heart)
+quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+basic.showIcon(IconNames.Happy)
+quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
+setup_Staff_ThisLessonOnly_Func()
+setup_System_AnyLessonAlways_Func()
 quest_Note_1.quest_Show_String_For_Note_Big_Func(
-"©️ 2024, 2025 Quest Institute. All rights reserved."
+"©️ 2025 Quest Institute. All rights reserved."
 )
 quest_Note_2.quest_Show_String_For_Note_Big_Func(
-"Your Code Can Start Below"
+"Student Setup-Code Can Start Below.."
 )
 basic.forever(function () {
 	
