4cef26a9da4d5f3e0b6245f4a465ce2f2b7d1f96
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-03-06 01:23:13 -0800

    25-0306-0120 *** Cleanup Startup Code

--- main.ts
+++ main.ts
@@ -1,23 +1,8 @@
-function setup_Staff_ThisLessonOnly_Func () {
-	
-}
-function setup_System_AnyLessonAlways_Func () {
-    quest_Dashboard.quest_Send_LoginOfBot_ToXrayDashboardOfMb_Func(
-    0,
-    quest_Toggle_OnOff_Enum.On,
-    quest_Debug_Show_Enum.Dashboard_OLED
-    )
-}
-function setup_Student_ThisLessonOnly_Func () {
-	
-}
-function pre_setup_System_AnyLessonAlways_Func () {
+function setup_For_Teacher_Func () {
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
-    "Fix 'show leds' Block's Orientation for Driving"
+    "Teacher's Setup Code... e.g. 'calibrate compass'"
     )
-    display.rotateTo(display.Direction.UpsideDown)
 }
-pre_setup_System_AnyLessonAlways_Func()
 quest_Note_1.quest_Show_String_For_Note_Small_Func(
 "2-Sequence Animation Validates New-Start"
 )
@@ -25,13 +10,12 @@ basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-setup_System_AnyLessonAlways_Func()
-setup_Staff_ThisLessonOnly_Func()
+setup_For_Teacher_Func()
 quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "©️ 2025 Quest Institute. All rights reserved."
 )
 quest_Note_2.quest_Show_String_For_Note_Big_Func(
-"Student Setup-Code Can Start Below.."
+"Below: Student can add more Setup Code"
 )
 basic.forever(function () {
 	
