aa48b4f7acb174c94a55b741cea54b06d6f33816
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-03-26 15:13:07 -0700

    robot100-blank_template (25-0326-1510, v1.2.0)

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-function setup_For_Teacher_Func () {
-    quest_Note_1.quest_Show_String_For_Note_Small_Func(
-    "Teacher's Setup Code... e.g. 'calibrate compass'"
-    )
-}
 quest_Note_1.quest_Show_String_For_Note_Small_Func(
 "2-Sequence Animation Validates New-Start"
 )
@@ -10,12 +5,14 @@ basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-setup_For_Teacher_Func()
-quest_Note_1.quest_Show_String_For_Note_Big_Func(
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"Setup Code for Teacher:"
+)
+quest_Note_6.quest_Show_String_For_Note_Small_Func(
 "©️ 2025 Quest Institute. All rights reserved."
 )
-quest_Note_2.quest_Show_String_For_Note_Big_Func(
-"Below: Student can add more Setup Code"
+quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"Setup Code for Student:"
 )
 basic.forever(function () {
 	
