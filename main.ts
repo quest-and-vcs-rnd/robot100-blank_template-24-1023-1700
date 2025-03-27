fce57d3b440eae301cf444a2bac30dc55d76ceda
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2025-03-26 19:28:04 -0700

    Ver 1.3.0: 25-0326-1930

--- main.ts
+++ main.ts
@@ -5,15 +5,24 @@ basic.showIcon(IconNames.Heart)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
 basic.showIcon(IconNames.Happy)
 quest_Timer.quest_Set_ContinueCurrentState_CountdownTimer_Func(2, quest_Time_Units_Enum.Seconds)
-quest_Note_1.quest_Show_String_For_Note_Small_Func(
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "Setup Code for Teacher:"
 )
-quest_Note_6.quest_Show_String_For_Note_Small_Func(
-"©️ 2025 Quest Institute. All rights reserved."
-)
 quest_Note_1.quest_Show_String_For_Note_Small_Func(
+"micro:bit's LED Display is upside-down on bot"
+)
+display.rotateTo(display.Direction.Normal)
+quest_Note_1.quest_Show_String_For_Note_Big_Func(
 "Setup Code for Student:"
 )
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    quest_Note_1.quest_Show_String_For_Note_Small_Func(
+    "Ver 1.3.0: 25-0326-1930"
+    )
+    quest_Note_1.quest_Show_String_For_Note_Small_Func(
+    "©️ 2025 Quest Institute. All rights reserved."
+    )
 })
